f8c5ca509c0342640a1be736239cb6a5e1e33fff
alex-jr-dev <geremia.alessandro@icloud.com>
2021-06-20 13:14:21 +0200

    Header implemented

---
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -0,0 +1,31 @@
+<header>
+    <div class="bg-blue">
+        <div class="container">
+            <div class="wrapper-text">
+                <p>Dc power visa<span>&reg;</span></p>
+                <p><a href="#">Additional dc sites <i class="fas fa-sort-down"></i></a></p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="nav">
+            <img src="{{asset ('images/dc-logo.png')}}" alt="DC logo">
+            <ul>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop <i class="fas fa-sort-down"></i></a></li>
+            </ul>
+            <form action="">
+                <input type="text" placeholder="Search"><i class="fas fa-search"></i>
+            </form>
+        </div>
+    </div>
+    </div>
+</header>
